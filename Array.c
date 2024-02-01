793a0d59754749d454f3a5aad941eefbf2a911e9
Satishvaishnav334 <satishvaishna@gmail.com>
2024-02-01 11:21:38 +0530

    Initial commit

---
+++ Array.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+        printf("Enter 5 Array = ");
+    getch();
+}
---
+++ s.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+#include<conio.h>
+int main()
+{
+        int a;
+        cout<<"Enter 5 Array = ";
+        cin>>a;
+
+}
